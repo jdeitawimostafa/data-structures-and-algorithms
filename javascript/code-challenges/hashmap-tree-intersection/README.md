7eed47352999b0d724316a846691f91cca4d35aa
jdeitawimostafa <jdeitawimostafa@gmail.com>
2021-06-28 15:45:58 +0300

    code 32 solved

---
+++ javascript/code-challenges/hashmap-tree-intersection/README.md
@@ -0,0 +1,17 @@
+# Feature Tasks
+
+* Find all values found to be in 2 binary trees : 
+
+1. Write a function called tree intersection
+2. Arguments: two binary trees
+3. Return: array 
+
+
+## Structure and Testing : 
+
+Utilize the Single-responsibility principle: any methods you write should be clean, reusable, abstract component parts to the whole challenge. You will be given feedback and marked down if you attempt to define a large, complex algorithm in one function definition. 
+
+
+### code challenge 32 whiteboard :
+
+![image](./whiteboard32.PNG)
---
+++ javascript/code-challenges/hashmap-tree-intersection/hashmap-tree-intersection.js
@@ -0,0 +1,119 @@
+'use strict';
+
+class Node{
+  constructor(value, left = null, right=null){
+    this.value=value;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class BinaryTree{
+  constructor(root = null){
+    this.root = root;
+  }
+
+  preOrder(){
+    let resultArray= [];
+
+    let traverse = (node) => {
+      resultArray.push(node.value);
+      if(node.left){
+        traverse(node.left);
+      }
+      if(node.right){
+        traverse(node.right);
+      }
+    };
+    traverse(this.root);
+    return resultArray;
+  }
+  
+  inOrder(){
+    let resultArray = [];
+    let traverse = (node) => {
+      if(node.left){
+        traverse(node.left);
+      }
+      resultArray.push(node.value);
+
+      if(node.right){
+        traverse(node.right);
+      }
+
+    };
+    traverse(this.root);
+    return resultArray;
+  }
+
+  postOrder(){
+    let resultArray = [];
+
+    let travrese = (node) => {
+      if(node.left){
+        travrese(node.left);
+      }
+      if(node.right){
+        travrese(node.right);
+      }
+      resultArray.push(node.value);
+    };
+    travrese(this.root);
+    return resultArray;
+  }
+
+  findmaximumvalue(binaryTreeArray){
+    let checkValue = binaryTreeArray[0];
+    for(let i=1; i<binaryTreeArray.length;i++){
+      if(binaryTreeArray[i] > checkValue){
+        checkValue = binaryTreeArray[i];
+      }
+    }
+    return checkValue;
+  }
+
+  breadthFirst(){
+    let breadthFirstArray = [];
+    let result = [];
+    if(this.root === null){
+      throw new Error('the tree is empty !');
+    } else {
+      breadthFirstArray.push(this.root);
+    }
+
+    while(breadthFirstArray.length !== 0){
+      let curr = breadthFirstArray.shift();
+      result.push(curr.value);
+      if(curr.left){
+        breadthFirstArray.push(curr.left);
+      }
+      if(curr.right){
+        breadthFirstArray.push(curr.right);
+      }
+    }
+    return result;
+  }
+}
+
+
+function treeIntersection(binary1,binary2){
+
+  if(!binary1 && !binary2) {
+    console.log('inside');
+    throw new Error('Exception');
+  }
+
+  let matchingvalues=[];
+
+  let binaryTree1=binary1.preOrder();
+  let binaryTree2= binary2.preOrder();
+
+  for (let i = 0; i < binaryTree1.length; i++) {
+    if(binaryTree1[i]===binaryTree2[i]){
+      matchingvalues.push(binaryTree1[i]);
+    }
+  }
+  return matchingvalues;
+}
+
+module.exports={BinaryTree:BinaryTree,Node:Node,treeIntersection:treeIntersection};
